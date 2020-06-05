fa95bb94442edbda808fba3f00307b09df7241a9
rvodopian <r.vodopian@spd-ukraine.com>
2020-06-05 12:35:01 +0300

    Add FutureModel

---
+++ src/main/java/com/spd/test/google_clone/model/FutureModel.java
@@ -0,0 +1,9 @@
+package com.spd.test.google_clone.model;
+
+public class FutureModel {
+    private final String futureModel =" new future model";
+
+    public String getFutureModel() {
+        return futureModel;
+    }
+}
