a1a537370e0671acfef82eca41df0a705d0195df
rvodopian <r.vodopian@spd-ukraine.com>
2020-06-05 12:51:51 +0300

    Add location in future model

---
+++ src/main/java/com/spd/test/google_clone/model/FutureModel.java
@@ -0,0 +1,11 @@
+package com.spd.test.google_clone.model;
+
+public class FutureModel {
+   private long id;
+    private final String futureModel =" new future model";
+
+    public String getFutureModel() {
+        return futureModel;
+    }
+
+}
